65fdbf42c83edb61fd2bb3fd487eb9fca500e460
Emerson <eemr3@yahoo.com.br>
2022-03-18 18:01:03 -0300

    feat: :zap: Cria função para validar o token

---
+++ middleware/validateToken.js
@@ -0,0 +1,11 @@
+const validateTokne = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+  if (authorization.length < 16 || authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  next();
+};
+
+module.exports = validateTokne;
