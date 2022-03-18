8d299d78336857df7d6e8293bfd820f2dea21669
Emerson <eemr3@yahoo.com.br>
2022-03-18 15:12:39 -0300

    feat: cria end-point para buscar uma conversar pelo id

--- index.js
+++ index.js
@@ -7,10 +7,19 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
 app.get('/talker', (req, res) => {
   const dataFile = readeFiles('./talker.json');
   if (dataFile.length > 0) return res.status(HTTP_OK_STATUS).json(dataFile);
   return res.status(HTTP_OK_STATUS).json([]);
+});
+
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const dataFile = readeFiles('./talker.json');
+  const result = dataFile.find((item) => item.id === Number(id));
+  if (!result) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(HTTP_OK_STATUS).json(result);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
