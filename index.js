3148c2417be1b094c44ac967be95d899fee0f2d5
Emerson <eemr3@yahoo.com.br>
2022-03-18 19:58:07 -0300

    feat: :fire: index.js
    Cria toda estrura lógica de chamada das rotas da app

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { readeFiles } = require('./services/fles');
-const { validateEmail, validatePassword } = require('./middleware/validateLogin');
-const genereteToken = require('./services/genereteToken');
+const talkerRouter = require('./routers/talkerRouter');
+const loginRouter = require('./routers/loginRouter');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,24 +9,9 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-app.get('/talker', (req, res) => {
-  const dataFile = readeFiles('./talker.json');
-  if (dataFile.length > 0) return res.status(HTTP_OK_STATUS).json(dataFile);
-  return res.status(HTTP_OK_STATUS).json([]);
-});
+app.use('/talker', talkerRouter);
 
-app.get('/talker/:id', (req, res) => {
-  const { id } = req.params;
-  const dataFile = readeFiles('./talker.json');
-  const result = dataFile.find((item) => item.id === Number(id));
-  if (!result) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  res.status(HTTP_OK_STATUS).json(result);
-});
-
-app.post('/login', validateEmail, validatePassword, (req, res) => { 
-  const token = genereteToken();
-  res.status(200).json({ token });
-});
+app.use('/login', loginRouter);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
