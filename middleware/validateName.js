133f7006e7c27c7d6fa52eda4618d59de3549cf4
Emerson <eemr3@yahoo.com.br>
2022-03-18 18:10:17 -0300

    fix: :bug: Corrige nome da propriedade

--- middleware/validateName.js
+++ middleware/validateName.js
@@ -1,11 +1,11 @@
 const validateName = (req, res, next) => {
   const { name } = req.body;
-  if (!name || name.lenght === 0) {
+  if (!name || name.length === 0) {
     res.status(400)
       .json({ message: 'O campo "name" é obrigatório' }); 
   }
 
-  if (name.lenght < 3) {
+  if (name.length < 3) {
     return res.status(400)
       .json({ message: 'O "name" deve ter pelo menos 3 caracteres' }); 
   }
