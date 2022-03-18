0a1814799b65c104045c89007b94438486b618df
Emerson <eemr3@yahoo.com.br>
2022-03-18 19:53:28 -0300

    feat: :zap: Feiles
    Cria lógica pra ler e criar arquivos

--- services/fles.js
+++ services/fles.js
@@ -1,8 +1,20 @@
 const fs = require('fs');
 
 const readeFiles = (file) => {
-  const resultReadFile = fs.readFileSync(file, 'utf8');
-  return JSON.parse(resultReadFile);
+  try {
+    const resultReadFile = fs.readFileSync(file, 'utf8');
+    return JSON.parse(resultReadFile);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-module.exports = { readeFiles };
+const createFiles = (file, text) => {
+  try {
+    return fs.writeFileSync(file, JSON.stringify(text));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { readeFiles, createFiles };
