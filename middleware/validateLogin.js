688b3b10b948bb6867f9b238287835664933c020
Emerson <eemr3@yahoo.com.br>
2022-03-18 19:56:08 -0300

    feat: :zap: Login
    Cria lógica do meddleware para validação do login

--- middleware/validateLogin.js
+++ middleware/validateLogin.js
@@ -1,6 +1,6 @@
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
-  console.log(email);
+
   const validEmail = /\S+@\S+\.\S+/; // código retirado do site https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
