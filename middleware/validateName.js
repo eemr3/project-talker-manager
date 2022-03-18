76ceef7244c2cd6873e14d6b2dd67f7eca99f29e
Emerson <eemr3@yahoo.com.br>
2022-03-18 19:56:39 -0300

    feat: :zap: Name
    Cria lógica do meddleware para validação do nome do palestrante

--- middleware/validateName.js
+++ middleware/validateName.js
@@ -4,7 +4,7 @@ const validateName = (req, res, next) => {
     res.status(400)
       .json({ message: 'O campo "name" é obrigatório' }); 
   }
-
+  
   if (name.length < 3) {
     return res.status(400)
       .json({ message: 'O "name" deve ter pelo menos 3 caracteres' }); 
