84d87d7f886f6d80d2d7dc136889f80371bd5608
Emerson <eemr3@yahoo.com.br>
2022-03-18 22:08:00 -0300

    refactor: :zap: ValidateTalk
    refator função que valida o rate

--- middleware/validateTalk.js
+++ middleware/validateTalk.js
@@ -6,25 +6,26 @@ const validateTalkObj = (req, res, next) => {
     return res.status(400)
        .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' }); 
      }
-   
-     if (typeof talk.rate === 'string' || talk.rate < 1 || talk.rate > 5) {
-      return res.status(400).json({
-        message: 'O campo "rate" deve ser um inteiro de 1 à 5',
-      });
-     }
+     if (talk.rate <= 0 || talk.rate > 5) {
+      console.log('ola');
+     return res.status(400).json({
+       message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+     });
+    }
   
   next();
 };
 
 const validateTalk = (req, res, next) => {
   const { talk } = req.body;
-  if (!talk || !talk.watchedAt || !talk.rate) {
-  return res.status(400).json({
+  
+  if (!talk || !talk.watchedAt || talk.rate === undefined) {
+    return res.status(400).json({
       message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     }); 
   }
+  
   validateTalkObj(req, res, next);
-
   next();
 };
 
